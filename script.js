fc0e0fecdd3f38a7a2f14590d8404fa94da526ef
astrsks <acsukaw@gmail.com>
2025-06-07 22:44:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ document.addEventListener('DOMContentLoaded', () => {
   const year = new Date().getFullYear();
   yearPara.textContent = `© ${year} Cy`;
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const carousel = document.querySelector('.carousel');
+  const prevBtn = document.querySelector('.carousel-btn.prev');
+  const nextBtn = document.querySelector('.carousel-btn.next');
+  
+
+  const gap = 24; 
+  const item = document.querySelector('.carousel-item');
+  const itemWidth = item.offsetWidth + gap;
+
+  prevBtn.addEventListener('click', () => {
+    carousel.scrollBy({ left: -itemWidth, behavior: 'smooth' });
+  });
+
+  nextBtn.addEventListener('click', () => {
+    carousel.scrollBy({ left: itemWidth, behavior: 'smooth' });
+  });
+});
